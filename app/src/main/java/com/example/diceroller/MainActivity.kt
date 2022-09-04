0078f3fb6377b031af92881b18eb177f4abb2e99
Cheng-Ying Li <newtaiwanstar@gmail.com>
2022-09-04 16:54:21 +0800

    feat: update imageView to dice_2.png after rolling

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -15,23 +15,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice(R.id.textView)
-            rollDice(R.id.textView2)
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
 
     /**
      * Roll the dice and update the screen with the result.
      */
-    private fun rollDice(textViewId: Int) {
+    private fun rollDice() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
-
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(textViewId)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 }
 
