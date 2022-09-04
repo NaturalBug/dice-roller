7facd6be77ad0cccb5978f65169ef1f88e0da97e
Cheng-Ying Li <newtaiwanstar@gmail.com>
2022-09-04 17:08:51 +0800

    feat: do a dice roll when the app starts

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -16,6 +16,9 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
+        // Do a dice roll when the app starts
+        rollDice()
     }
 
     /**
